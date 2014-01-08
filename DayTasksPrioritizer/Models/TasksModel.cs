f74d44bfcbacc3b3c7ec3c6ec25c79c9838d053e
Saulius <saulius.sunauskas@gmail.com>
2014-01-08 19:36:48 +0200

    Add missing TasksModel.cs file

---
+++ DayTasksPrioritizer/Models/TasksModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace DayTasksPrioritizer.Models
+{
+    public class TasksModel
+    {
+        public List<TaskModel> TasksList { get; set; }
+
+        public TasksModel()
+        {
+            TasksList = new List<TaskModel>();
+        }
+    }
+}
